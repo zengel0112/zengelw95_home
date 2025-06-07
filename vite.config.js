09ab175f03a8cfc03033f92cbcc5691b7c02b4a6
Zengelma <zorotsengel@gmail.com>
2025-06-08 00:45:41 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // if deploying to https://username.github.io/your-repo-name/
-  plugins: [react()],
+  base: '/',
+  plugins: [react()]
 })
